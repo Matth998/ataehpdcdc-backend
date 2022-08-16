c7a7b76aaedf39a97f3eca43a08ef9a5c5221339
Matheus Da Silva <85530101+Matth998@users.noreply.github.com>
2022-08-16 02:40:10 -0300

    Add files via upload

---
+++ src/main/java/com/gruop/ataehpdcdc/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.gruop.ataehpdcdc.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gruop.ataehpdcdc.model.UserModel;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+
+	public Optional<UserModel> findByEmail(String email);
+
+	public List<UserModel> findAllByNameContainingIgnoreCase(String name);
+
+}
---
+++ src/test/java/com/gruop/ataehpdcdc/AtaehpdcdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruop.ataehpdcdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtaehpdcdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
