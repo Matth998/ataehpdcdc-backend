57f0d260d1caa5f9986072f9f6a20aa334ef9ceb
Matheus Da Silva <85530101+Matth998@users.noreply.github.com>
2022-08-16 02:21:10 -0300

    Created BackEnd Ataehpdcdc
    Project Ataehpdcdc

---
+++ ataehpdcdc/src/main/java/com/gruop/ataehpdcdc/AtaehpdcdcApplication.java
@@ -0,0 +1,13 @@
+package com.gruop.ataehpdcdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtaehpdcdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtaehpdcdcApplication.class, args);
+	}
+
+}
